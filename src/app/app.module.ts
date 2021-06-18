5a307520eacfa40f1a917ea6214dc7d752584398
Tariq Khan <newtechtariq@gmail.com>
2021-06-19 02:18:33 +0530

    Added cart, product and women page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { TopheaderComponent } from './topheader/topheader.component';
 import { BottomfooterComponent } from './bottomfooter/bottomfooter.component';
+import { WomenComponent } from './women/women.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
@@ -16,15 +19,21 @@ import { BottomfooterComponent } from './bottomfooter/bottomfooter.component';
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomepageComponent },
-
+      { path: 'home', component: HomepageComponent },
+      { path: 'women', component: WomenComponent },
+      { path: 'product', component: ProductComponent },
+      { path: 'cart', component: CartComponent },
+      { path: '**', component: HomepageComponent },
     ])
   ],
   declarations: [
     AppComponent,
     HomepageComponent,
     TopheaderComponent,
-    BottomfooterComponent
+    BottomfooterComponent,
+    WomenComponent,
+    ProductComponent,
+    CartComponent
   ],
   bootstrap: [
     AppComponent
